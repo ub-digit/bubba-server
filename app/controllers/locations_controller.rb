0cfeab95d3b2ccaa0df1f616085abc73feb7e61d
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2015-04-16 15:43:09 +0200

    Demand active objects for libary

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,6 @@
 class LocationsController < ApplicationController
   def index
-    locations = Location.where("sort_order IS NOT NULL").where(id: BookingObject.pluck(:location_id))
+    locations = Location.where("sort_order IS NOT NULL").where(id: BookingObject.where(active: true).pluck(:location_id))
     render json: {locations: locations}
   end
 end
