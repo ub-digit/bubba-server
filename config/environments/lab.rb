8c24fe23645c72a3d66e2bbcecc6aa03bd97df83
Lars berglén <lars.berglen@ub.gu.se>
2021-03-03 14:16:57 +0100

    Changed log to stdout

--- config/environments/lab.rb
+++ config/environments/lab.rb
@@ -46,6 +46,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
+  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
